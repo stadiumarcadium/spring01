a98c79d35f3ad5d28dd84f9acd0bbaa843f04c68
StadiumArcadium <stdmarcdm@gmail.com>
2022-09-02 23:16:09 +0300

    HomeWork 07

--- lesson4-spring-boot/src/main/java/ru/mal/ProductRepository.java
+++ lesson4-spring-boot/src/main/java/ru/mal/ProductRepository.java
@@ -1,52 +1,16 @@
 package ru.mal;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicLong;
+import java.math.BigDecimal;
 
-@Repository
-public class    ProductRepository {
-    private final Map<Long, Product> productMap = new ConcurrentHashMap<>();
-
-    private final AtomicLong identity = new AtomicLong(0);
-
-    @PostConstruct
-    public void init() {
-        this.insert(new Product("Product 1"));
-        this.insert(new Product("Product 2"));
-        this.insert(new Product("Product 3"));
-        this.insert(new Product("Product 4"));
-        this.insert(new Product("Product 5"));
-    }
-
-    public void insert(Product product) {
-        long id = identity.incrementAndGet();
-        product.setId(id);
-        productMap.put(id, product);
-    }
-
-    public Product save(Product product) {
-        if (product.getId() == null) {
-            product.setId(identity.incrementAndGet());
-        }
-        productMap.put(product.getId(), product);
-        return product;
-    }
-
-    public List<Product> findAll() {
-        return new ArrayList<>(productMap.values());
-    }
-
-    public Product findById(long id) {
-        return productMap.get(id);
-    }
-
-    public void delete(long id) {
-        productMap.remove(id);
-    }
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Query(value = """
+            select * from products u
+            where (:minCostFilter is null or u.cost < :minCostFilter)
+            and (:maxCostFilter is null or u.cost > :maxCostFilter)
+            """, nativeQuery = true)
+    Page<Product> productsByFilter(BigDecimal minCostFilter, BigDecimal maxCostFilter);
 }
