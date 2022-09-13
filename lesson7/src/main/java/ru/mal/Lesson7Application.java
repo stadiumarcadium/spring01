4e25987be253eb3be6b0734f2b93a0b555d9c6a7
stadiumarcadium <stdmarcdm@gmail.com>
2022-09-14 01:15:58 +0300

    homework 7 и 8 добавил фильтрацию по имени и стоимости добавил разбивку товаров по 4 на страницу добавил прослойку сервис и дто

---
+++ lesson7/src/main/java/ru/mal/Lesson7Application.java
@@ -0,0 +1,13 @@
+package ru.mal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson7Application.class, args);
+    }
+
+}
---
+++ lesson7/src/main/java/ru/mal/model/Product.java
@@ -0,0 +1,28 @@
+package ru.mal.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "products")
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private BigDecimal cost;
+
+    public Product(String title) {
+        this.title = title;
+    }
+}
